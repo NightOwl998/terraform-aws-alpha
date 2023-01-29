fe252579eed1eed3d9923ef654c417527863b7d6
NighOwl998 <gf_hasnaoui@esi.dz>
2023-01-29 18:44:36 +0100

    Applying D.R.Y by using count

--- main.tf
+++ main.tf
@@ -1,46 +1,43 @@
+locals{
+ vpc_cidr="192.168.0.0/16"
+ public_cidr=["192.168.0.0/24","192.168.1.0/24"]
+ private_cidr=["192.168.2.0/24","192.168.3.0/24"]
+ availibilty_zones=["us-west-2a","us-west-2b"]
+
+}
+
+
 resource "aws_vpc" "main" {
-  cidr_block       = "192.168.0.0/16"
+  cidr_block       = local.vpc_cidr
   
   tags = {
     Name = "myVPC"
   }
 }
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "public" {
+  count=length(local.public_cidr)
+
   vpc_id     = aws_vpc.main.id
-  cidr_block = "192.168.0.0/24"
-  availability_zone="us-west-2a" 
+  cidr_block = local.public_cidr[count.index]
+  availability_zone=local.availibilty_zones[count.index]
 
   tags = {
-    Name = "public1"
+    Name = "public${count.index+1}"
   }
 }
-resource "aws_subnet" "private1" {
+resource "aws_subnet" "private" {
+  count=length(local.private_cidr)
+
   vpc_id     = aws_vpc.main.id
-  cidr_block = "192.168.1.0/24"
-  availability_zone="us-west-2a" 
+  cidr_block = local.private_cidr[count.index]
+
+  availability_zone=local.availibilty_zones[count.index]
 
   tags = {
-    Name = "private1"
+    Name = "private${count.index+1}"
   }
 }
-resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "192.168.2.0/24"
-  availability_zone="us-west-2b" 
 
-  tags = {
-    Name = "public2"
-  }
-}
-resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "192.168.3.0/24"
-  availability_zone="us-west-2b" 
-
-  tags = {
-    Name = "private2"
-  }
-}
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
@@ -48,6 +45,7 @@ resource "aws_internet_gateway" "gw" {
     Name = "myGateway"
   }
 }
+
 resource "aws_route_table" "r_p" {
   vpc_id = aws_vpc.main.id
 
@@ -60,58 +58,51 @@ resource "aws_route_table" "r_p" {
     Name = "public_route_table"
   }
 }
-resource "aws_route_table_association" "a1" {
-  subnet_id      = aws_subnet.public1.id
+resource "aws_route_table_association" "a" {
+  count=length(local.public_cidr)
+
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.r_p.id
-}
-resource "aws_route_table_association" "a2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.r_p.id
-}
-resource "aws_eip" "eip1" {
-  vpc = true
-}
-resource "aws_eip" "eip2" {
-  vpc = true
-}
-resource "aws_nat_gateway" "gwNat1" {
-  allocation_id = aws_eip.eip1.id
-  subnet_id     = aws_subnet.public1.id
-}
-resource "aws_nat_gateway" "gwNat2" {
-  allocation_id = aws_eip.eip2.id
-  subnet_id     = aws_subnet.public2.id
 }
 
-resource "aws_route_table" "r_Nat1" {
+resource "aws_eip" "eip" {
+  count=length(local.public_cidr)
+
+  vpc = true
+
+  
+}
+
+resource "aws_nat_gateway" "gwNat" {
+  count=length(local.public_cidr)
+
+  allocation_id = aws_eip.eip[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  
+}
+
+
+
+resource "aws_route_table" "r_Nat" {
+
+  count=length(local.private_cidr)
+
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.gwNat1.id
+    gateway_id = aws_nat_gateway.gwNat[count.index].id
   }
 
     tags = {
-    Name = "private1_route_table"
+    Name = "private_route_table${count.index+1}"
   }
 }
-resource "aws_route_table_association" "a_pr1" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.r_Nat1.id
-}
-resource "aws_route_table" "r_Nat2" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table_association" "a_pr" {
+  count=length(local.private_cidr)
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.gwNat2.id
-  }
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.r_Nat[count.index].id
+}
 
-    tags = {
-    Name = "private2_route_table"
-  }
-}
-resource "aws_route_table_association" "a_pr2" {
-  subnet_id      = aws_subnet.private2.id
-  route_table_id = aws_route_table.r_Nat2.id
-}
