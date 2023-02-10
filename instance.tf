6b851899144f967e939c085dd85eb2cd4e5bedc1
fadia <fadia@XLIM05040.sic.univ-poitiers.fr>
2023-02-10 16:30:18 +0100

    Created 2 ec2 instance

---
+++ instance.tf
@@ -0,0 +1,75 @@
+resource "aws_instance" "public" {
+  ami                     = "ami-06e85d4c3149db26a"
+  instance_type           = "t2.micro"
+  subnet_id               = aws_subnet.public[0].id
+  key_name                = "main"
+  vpc_security_group_ids  = [aws_security_group.public.id]
+  associate_public_ip_address = true
+  tags = {
+    name = "${var.env_code}public_instance"
+  }
+}
+resource "aws_instance" "private" {
+  ami                     = "ami-06e85d4c3149db26a"
+  instance_type           = "t2.micro"
+  subnet_id               =aws_subnet.private[0].id
+  key_name                = "main"
+  vpc_security_group_ids  = [aws_security_group.private.id]
+  tags = {
+    name = "${var.env_code}private_instance"
+  }
+}
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}public"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh from our lan"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["${var.myIpAddress}/24"]
+    
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}public_sg"
+  }
+}
+resource "aws_security_group" "private" {
+   name        = "${var.env_code}private"
+  description = "Allow ssh inbound traffic from inside the vpc"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}private_sg"
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -3,3 +3,4 @@ variable "vpc_cidr" {}
 variable "public_cidr" {}
 variable "private_cidr" {}
 variable "availibilty_zones" {}
+variable "myIpAddress" {}
