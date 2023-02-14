c6d21e8a66acdc77c1777863afc7ac9e484fcfb0
fadia <fadia@XLIM05040.sic.univ-poitiers.fr>
2023-02-14 16:18:59 +0100

    added data.tf file

---
+++ data.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20230119.1-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["137112412989"]
+}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
